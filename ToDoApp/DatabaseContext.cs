6eb2af03aad41db86645c4852710c10ba3693276
Jakub Olech <itsjakeoley@gmail.com>
2019-07-28 17:27:11 +0200

    Database setup, changed class name of MainWindow to MainView

---
+++ ToDoApp/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using SQLite.CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+            : base("name=DatabaseModel")
+        { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new SqliteCreateDatabaseIfNotExists<DatabaseContext>(modelBuilder));
+        }
+
+        public virtual DbSet<TaskEntity> Tasks { get; set; }
+    }
+}
---
+++ ToDoApp/TaskEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp
+{
+    [Table("Tasks")]
+    public class TaskEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+    }
+}
